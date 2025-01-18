2b9253eb95c7d7cc47e3c0f0390a0302056d072d
Bolin <Bolin8621@gmail.com>
2025-01-18 13:09:14 +0800

    Base quacker and tile system

---
+++ Levels/LevelManager.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+@onready var terrain: TileMapLayer = $Terrain
+
+var walls: Array[Vector2i]
+var quacker: Quacker
+
+var level_size: Vector2
+var tile_size: Vector2 = Vector2(64, 64);
+
+
+func _ready():
+	level_size = terrain.get_used_rect().size
+	walls = terrain.get_used_cells_by_id(0)
+	var quacker_tile_coord = terrain.get_used_cells_by_id(1)[0]
+	terrain.set_cell(quacker_tile_coord, -1)  # Set to -1 to remove the tile
+	var quacker_world_position = tile_to_world(quacker_tile_coord)
+	quacker = Quacker.summonQuacker()
+	quacker.global_position = quacker_world_position
+	get_parent().add_child.call_deferred(quacker)
+
+func _input(event):
+	if not(event is InputEventKey and event.pressed):
+		return
+		
+	if event.is_action("up"):
+		quackerMove(Vector2.UP)
+	elif event.is_action("down"):
+		quackerMove(Vector2.DOWN)
+	elif event.is_action("left"):
+		quackerMove(Vector2.LEFT)
+	elif event.is_action("right"):
+		quackerMove(Vector2.RIGHT)
+
+
+func quackerMove(direction: Vector2i):
+	var anyMoved = false;
+	var tile_coords = world_to_tile(quacker.position)
+	var target = tile_coords + direction
+	print("Moving in ", direction)
+	if target in walls:
+		return false
+	else:
+		var target_in_world = tile_to_world(target)
+		var moved = quacker.move(target_in_world)
+		if moved:
+			anyMoved = true;
+
+
+### Utility
+func restart_level():
+	var t = get_tree()
+	if t:
+		t.reload_current_scene()
+	else:
+		pass
+
+func world_to_tile(world_pos: Vector2):
+	var local = terrain.to_local(world_pos)
+	var tile = terrain.local_to_map(local)
+	return tile
+
+func tile_to_world(tile_coords: Vector2):
+	var local = terrain.map_to_local(tile_coords)
+	var global = terrain.to_global(local)
+	return global
---
+++ Objects/Quacker/Quacker.gd
@@ -0,0 +1,29 @@
+class_name Quacker
+extends Node2D
+
+static var QuackerScene = load("res://Objects/Quacker/Quacker.tscn")
+
+@onready var sprite: Sprite2D = $Sprite
+
+var movementTween
+var tween_duration = 0.1
+
+static func summonQuacker():
+	var quacker = QuackerScene.instantiate()
+	return quacker
+	
+func move(target: Vector2):
+	tweenToTarget(target, tween_duration)
+	return true
+
+func tweenToTarget(target_position: Vector2, duration: float):
+	movementTween = get_tree().create_tween()
+	movementTween.tween_property(
+		self,
+		"global_position",
+		target_position,
+		duration
+	)
+	movementTween.set_trans(Tween.TRANS_LINEAR)
+	movementTween.set_ease(Tween.EASE_OUT)
+	
